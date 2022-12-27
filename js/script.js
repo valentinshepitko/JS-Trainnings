f21c763e18d513de4684b90a6334cc0240d01cf8
Valentin Shepitko <loungestudiopo@gmail.com>
2022-12-27 18:53:21 -0500

    1st commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// alert('HIIII');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?" "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Ваше имя', '');
+answers[1] = prompt('Ваша фамилия', '');
+answers[2] = prompt('Ваш возраст', '');
+
+document.write(answers);
